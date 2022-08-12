832edf86b9c2595dd553fb28757cc38d867e7e7c
Elliott Z <elliott.zwertvaegher@3i.ai>
2022-08-12 11:03:13 +0900

    Initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,21 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      eslintrc: { enabled: true },
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({ dts: true, resolvers: [ElementPlusResolver()] }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
